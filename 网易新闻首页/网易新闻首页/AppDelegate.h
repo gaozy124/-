3982a26e2b9f772978afc4e76b8333aef41a7262
gzy <298012895@qq.com>
2016-02-07 13:37:45 +0800

    初始化项目

---
+++ 网易新闻首页/网易新闻首页/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易新闻首页
+//
+//  Created by 高志宇 on 16/2/7.
+//  Copyright © 2016年 高志宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 网易新闻首页/网易新闻首页/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  网易新闻首页
+//
+//  Created by 高志宇 on 16/2/7.
+//  Copyright © 2016年 高志宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
